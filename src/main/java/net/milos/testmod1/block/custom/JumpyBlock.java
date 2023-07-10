f818f2c76db3027c76b71e9bcf866845af3bde7a
JabeJabeJabe <misa.vojin@gmail.com>
2023-07-10 18:30:47 +0200

    sedmi video gotov

--- src/main/java/net/milos/testmod1/block/custom/JumpyBlock.java
+++ src/main/java/net/milos/testmod1/block/custom/JumpyBlock.java
@@ -1,19 +1,26 @@
 package net.milos.testmod1.block.custom;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class JumpyBlock extends Block {
 
@@ -24,17 +31,29 @@ public class JumpyBlock extends Block {
     @Override
     public InteractionResult use(BlockState state, Level level, BlockPos pos,
                                  Player player, InteractionHand hand, BlockHitResult result) {
-        player.sendSystemMessage(Component.literal("Right clicked Jumpy Block!"));
+        if (!level.isClientSide && hand == InteractionHand.MAIN_HAND){
+            player.sendSystemMessage(Component.literal("Right clicked Jumpy Block!"));
+        }
         return super.use(state, level, pos, player, hand, result);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable BlockGetter getter, List<Component> components, TooltipFlag flag) {
+        if (Screen.hasShiftDown()){
+            components.add(Component.literal("Step on this block to recieve jump boost!!a"));
+        } else {
+            components.add(Component.literal("Press SHIFT for mor info")
+                    .withStyle(ChatFormatting.UNDERLINE).withStyle(ChatFormatting.YELLOW));
+        };
+
+        super.appendHoverText(stack, getter, components, flag);
     }
 
     @Override
     public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
         if (entity instanceof LivingEntity livingEntity){
-            livingEntity.addEffect(new MobEffectInstance(MobEffects.JUMP, 2));
+            livingEntity.addEffect(new MobEffectInstance(MobEffects.JUMP, 2, 3));
         }
-
-
         super.stepOn(level, pos, state, entity);
     }
 
--- src/main/java/net/milos/testmod1/item/custom/SixtyNineBallItem.java
+++ src/main/java/net/milos/testmod1/item/custom/SixtyNineBallItem.java
@@ -1,13 +1,22 @@
 package net.milos.testmod1.item.custom;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class SixtyNineBallItem extends Item {
 
@@ -20,13 +29,32 @@ public class SixtyNineBallItem extends Item {
                                                   InteractionHand hand) {
         if (!level.isClientSide && hand == InteractionHand.MAIN_HAND){
             outputRandomNumber(player);
-            player.getCooldowns().addCooldown(this, 20);
+            player.getCooldowns().addCooldown(this, 9);
         }
         return super.use(level, player, hand);
     }
 
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+        if (Screen.hasShiftDown()){
+            components.add(Component.literal("Use this to get a random number xD (something good if you get a good number)"));
+        } else {
+            components.add(Component.literal("Press SHIFT for mor info")
+                    .withStyle(ChatFormatting.STRIKETHROUGH).withStyle(ChatFormatting.DARK_GREEN));
+        };
+        super.appendHoverText(stack, level, components, flag);
+    }
+
     private void outputRandomNumber(Player player){
-        player.sendSystemMessage(Component.literal("Your number is "+getRandomNumber()));
+        int randomnumber = getRandomNumber();
+        if (randomnumber == 69){
+            player.sendSystemMessage(Component.literal("leeejjjj dobio si "+ randomnumber));
+            player.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 60, 10));
+            player.addEffect(new MobEffectInstance(MobEffects.SATURATION, 60, 10));
+        }
+        else {
+            player.sendSystemMessage(Component.literal("Your number is "+ randomnumber));
+        }
     }
 
     private int getRandomNumber(){
