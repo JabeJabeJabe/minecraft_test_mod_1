c3136bd9dfb663bed712ef9d3015d0a7ab7c05fb
JabeJabeJabe <misa.vojin@gmail.com>
2023-07-10 14:11:15 +0200

    video 5 gotov, advanced item

--- src/main/java/net/milos/testmod1/item/ModItems.java
+++ src/main/java/net/milos/testmod1/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.milos.testmod1.item;
 
 import net.milos.testmod1.TestMod1;
+import net.milos.testmod1.item.custom.SixtyNineBallItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -15,9 +16,12 @@ public class ModItems {
 
     public static final RegistryObject<Item> GUMMO = ITEMS.register("gummo",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TESTMOD1_TAB)));
-
     public static final RegistryObject<Item> RAW_GUMMO = ITEMS.register("raw_gummo",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TESTMOD1_TAB)));
+
+
+    public static final RegistryObject<Item> SIXTY_NINE_BALL = ITEMS.register("sixty_nine_ball",
+            () -> new SixtyNineBallItem(new Item.Properties().tab(ModCreativeModeTab.TESTMOD1_TAB).stacksTo(1)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
